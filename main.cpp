41995c381e346f83ef94a04272390647b492d515
Eterxy <JONATHAN.ZOU47@myhunter.cuny.edu>
2021-03-13 17:15:52 +0000

    I added in the constructor and the codes for the operator overloading functions

--- main.cpp
+++ main.cpp
@@ -8,13 +8,59 @@
 class Rot13 {
     const int rotation{13};
     std::string text;
-
 public:
-    Rot13(std::string msg = "");
-    bool operator!();
-    void operator>>(std::string&);
-    friend void operator<<(std::string&, Rot13&);
+  Rot13(std::string msg = ""){
+    unsigned int idx{0};
+    for (idx = 0; idx < msg.length(); idx++){
+      if ((((static_cast<int>(msg[idx]) <= 122) && (static_cast<int>(msg[idx]) >= 97)) || (static_cast<int>(msg[idx]) == 32)) )
+        text += msg[idx];
+      else{
+        text.erase(0,idx);
+        break;
+      }
+    }
+  }
+
+  bool operator!(){
+    return text.length() == 0;
+  }
+
+  void operator>>(std::string& out){
+    int code;
+    for (unsigned int j = 0; j < text.length(); j++){
+      code = static_cast<int>(text[j]);
+      if (code == 32){
+        out += static_cast<char>(code);
+        continue;
+      }
+      code += rotation;
+      if (code > 122){
+        code -= 122;
+        code += 96;
+      }
+      out += static_cast<char>(code);
+    }
+  }
+  
+  friend void operator<<(std::string&, Rot13&);
 };
+
+void operator<<(std::string& abc, Rot13& d){
+  int back;
+  for (unsigned int k = 0; k < d.text.length(); k++){
+    back = static_cast<int>(d.text[k]);
+    if (back == 32){
+      abc += static_cast<char>(back);
+      continue;
+    }
+    back = back - d.rotation;
+    if (back < 97){
+      back = 97 - back;
+      back = 123 - back;
+    }
+    abc += static_cast<char>(back);
+  }
+}
 
 
 //------------------------------
